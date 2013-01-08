7f2dc8202c43e2d89d4f91f9c4b20c7ab04d7e40
Schalk.Wolhuter <Schalk.Wolhuter@SchalkWolhuter.Pilog.co.za>
2013-01-08 09:47:54 +0200

    Trying out this git.

---
+++ GitTestProject/src/com/pilog/git/gitTestClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pilog.git;
+
+/**
+ *
+ * @author Schalk.Wolhuter
+ */
+public class gitTestClass
+{
+    
+}
